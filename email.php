82040871b5d887a80bafca55f02a2bb230dbe95c
Mirna <mirnarecinos382@gmail.com>
2024-03-31 22:46:49 -0300

    Modificación de la lógica del envío del correo electrónico

--- email.php
+++ email.php
@@ -2,10 +2,28 @@
 
 //Todo obligatorio menos el header
 $to = "lr22003@esfe.agape.edu.sv";
+
+/* Multiples correos
+$to = ["lr22003@esfe.agape.edu.sv", "otrocorreo"]; */
+
 /* El título del correo (ASUNTO)*/
-$subject = "Este es un mail de prueba";
-$message = "Hola, estoy probando el envío de correos";
+$subject = "RESERVACIONES";
+
+/* $message = "Hola, estoy probando el envío de correos"; */
+$message = file_get_contents('./template.html');
+
 $headers = "From: mirnarecinos382@gmail.com";
+// Un nuevo valor a la variable
+$headers .="MIME-Version: 1.0\r\n";// Protocolo para interpretar el html
+$headers.= "Content-Type: text/html; charset=UTF-8\r\n";
+
+/* Para enviar a multiples correos
+foreach($to as $recipient){
+    // Enviar muchos correos a la misma dirección metemos dentro del for.
+    for($i=0; $i < 3; $i++){
+    mail($recipient, $subject, $message, $headers);
+    }
+} */
 
 mail($to, $subject, $message, $headers);
 echo "Email enviado";
